244d775652c37a22e661b48935e0d1b96c0762d2
Dan Davison <dan@counsyl.com>
2015-10-17 00:01:42 -0700

    Output has_foreign_key flag

--- sql_data_generator/generate_data.py
+++ sql_data_generator/generate_data.py
@@ -1,16 +1,30 @@
 from sqlparse import parse
 from sqlparse import sql
+from sqlparse import tokens
 
 
 def generate_data(text):
     statements = parse(text)
     for create_table_stmnt in get_create_table_statements(statements):
-        print create_table_stmnt.get_name()
-        for column in get_columns(create_table_stmnt):
-            print "\t", column
+        table_name = create_table_stmnt.get_name()
+        columns, has_foreign_key = get_columns(create_table_stmnt)
+        print table_name, '*' if has_foreign_key else ''
+        for column in columns:
+            print '\t', column
 
 
 def get_columns(create_table_stmnt):
+    has_foreign_key = False
+    columns = []
+    for column in _get_columns(create_table_stmnt):
+        if is_foreign_key_info(column):
+            has_foreign_key = True
+        else:
+            columns.append(column)
+    return columns, has_foreign_key
+
+
+def _get_columns(create_table_stmnt):
     for tok1 in create_table_stmnt.tokens:
         if isinstance(tok1, sql.Function):
             for tok2 in tok1.tokens:
@@ -22,12 +36,20 @@ def get_columns(create_table_stmnt):
                             for tok4 in tok3.tokens:
                                 if isinstance(tok4, sql.Identifier):
                                     yield tok4
+                                elif isinstance(tok4, sql.Token):
+                                    if tok4.ttype == tokens.Keyword:
+                                        if is_foreign_key_info(tok4):
+                                            yield tok4
 
 
 def get_create_table_statements(statements):
     for stmnt in statements:
         if str(stmnt).strip().startswith('CREATE TABLE'):
             yield stmnt
+
+
+def is_foreign_key_info(token):
+    return token.value == 'FOREIGN'
 
 
 if __name__ == '__main__':
